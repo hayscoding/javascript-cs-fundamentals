55faa05b2e838a3c593e23ce931a07cb32c62142
Hays Stanford <stanfordhays@gmail.com>
2020-01-28 13:57:31 -0600

    added code to create LinkedList & add nodes:

---
+++ data-structures/linked-list.js
@@ -0,0 +1,38 @@
+function Node {
+    this.data = data;
+    this.next = null;
+}
+
+class LinkedList{
+    constructor() {
+        this.head = null;   //  Node at start of list
+        this.tail = null;   //  Node at end of list
+    }
+
+    addNode(data) {
+        const node = new Node(data);   
+
+        if(!this.head) {
+            //  Make this node both the tail & head
+            //  since there are no other nodes.
+            this.head = node;
+            this.tail = node;
+        } else {
+            //  Make this the next node for the current tail
+            this.tail.next = node;
+            //  Then set the tail to this node
+            this.tail = node;
+        }
+    }
+
+    //  Calls cb(node) for all nodes, starting with head
+    traverse(cb) {
+        let current = this.head
+        
+        if(cb)  //  Only loop if a function was passed
+            while (current) {
+                cb(current)
+                current = current.next  //  Move to next node
+            }
+    }
+}
