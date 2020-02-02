a53eca4fbeb8f9b6f5e0fe1f5c879d393bc6638b
Hays Stanford <stanfordhays@gmail.com>
2020-02-01 18:45:54 -0600

    created add() for Tree class

--- data-structures/tree.js
+++ data-structures/tree.js
@@ -7,6 +7,23 @@ class Tree {
     constructor() {
         this.root = null;
     }
+
+    add(data, toNodeData) {
+        const node = new Node(data);
+        // If the toNodeData arg is passed, find it. Otherwise, store null.
+        const parent = toNodeData ? this.findBFS(toNodeData) : null;
+
+        // Push new node to parent with passed toNodeData in its data
+        if(parent) {
+            parent.children.push(node)
+        } else {
+            // If there's no parent, make this the root node
+            if(!this.root)
+                parent.children.push(node);
+            else
+                return "Tried to store node as root when root already exists."
+        }
+    }
 }
 
 (function test() {
