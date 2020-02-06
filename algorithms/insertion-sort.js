81c281edc59a8ba70c6b8079ddb85dcfb435eac5
Hays Stanford <stanfordhays@gmail.com>
2020-02-06 14:10:29 -0600

    added descriptive comments to data structure

--- algorithms/insertion-sort.js
+++ algorithms/insertion-sort.js
@@ -3,13 +3,21 @@ function insertionSort(array) {
         let temp = array[i];
         let j = i - 1;
 
+        console.log(`i: ${i}, temp: ${temp}`)
+        
+        // Moves temp to position where it is less than
+        // all the numbers in front of its index
         while(array[j] > temp) {
-            array[j+1] = array[j];
+            array[j+1] = array[j];  // Copy value of j to next index in front of it
 
-            j--;
+            j--; // Decrement j to see if more values should be swapped
         }
 
-        array[j + 1] = temp;
+        // The loop breaks after decrementing j, so undo the final decrement
+        j++; 
+
+        // j is the index when temp is greater than all numbers before it
+        array[j] = temp; 
     }
 
     return array
@@ -21,6 +29,6 @@ function insertionSort(array) {
     const testArray3 = [2, 1]
 
     console.log(insertionSort(testArray1))
-    console.log(insertionSort(testArray2))
-    console.log(insertionSort(testArray3))
+    // console.log(insertionSort(testArray2))
+    // console.log(insertionSort(testArray3))
 })()
