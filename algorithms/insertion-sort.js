426c89e76bac345336f8761a96dc87710116b829
Hays Stanford <stanfordhays@gmail.com>
2020-02-06 13:51:26 -0600

    fully implemented insertionSort() algorithm

--- algorithms/insertion-sort.js
+++ algorithms/insertion-sort.js
@@ -1,4 +1,17 @@
 function insertionSort(array) {
+    for(let i = 1; i < array.length; i++) {
+        let temp = array[i];
+        let j = i - 1;
+
+        while(array[j] > temp) {
+            array[j+1] = array[j];
+
+            j--;
+        }
+
+        array[j + 1] = temp;
+    }
+
     return array
 }
 
