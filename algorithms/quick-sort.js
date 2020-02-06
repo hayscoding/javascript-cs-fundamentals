169677844f39e4ce904f144d873cc92aef9eeb47
Hays Stanford <stanfordhays@gmail.com>
2020-02-06 15:22:17 -0600

    implemented partitionHoare() to work as intended

--- algorithms/quick-sort.js
+++ algorithms/quick-sort.js
@@ -15,10 +15,32 @@ function quickSort(array, left, right) {
     return array;
 }
 
+function partitionHoare(array, left, right) {
+    const pivot = Math.floor((left + right) / 2);
+
+    while(left <= right) {
+        while(array[left] < array[pivot]) {
+            left++;
+        }
+
+        while(array[right] > array[pivot]) {
+            right--;
+        }
+
+        if (left <= right) {
+            [array[left], array[right]] = [array[right], array[left]];
+            left++;
+            right--;
+        }
+    }
+
+    return left;
+}
+
 (function test() {
     const testArray1 = [4, 5, 2, 1, 3, 8]
     const testArray2 = [5, 5, 6, 100, 3, 5, 2, 1, 5, 7, 8888, 4]
     const testArray3 = [2, 1]
 
-    console.log('test')
+    console.log(quickSort(testArray1))
 })()
