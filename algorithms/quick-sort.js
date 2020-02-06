f4bb665eed66860ba3e10afc683eafa6bdc9b4fb
Hays Stanford <stanfordhays@gmail.com>
2020-02-06 15:05:26 -0600

    defined quickSort() in full.

--- algorithms/quick-sort.js
+++ algorithms/quick-sort.js
@@ -2,6 +2,17 @@ function quickSort(array, left, right) {
     left = left || 0;
     right = right || array.length - 1;
 
+    const pivot = partitionHoare(array, left, right);
+
+    if(left < pivot - 1) {
+        quickSort(array, left, pivot - 1);
+    }
+
+    if(right > pivot) {
+        quickSort(array, pivot, right);
+    }
+
+    return array;
 }
 
 (function test() {
