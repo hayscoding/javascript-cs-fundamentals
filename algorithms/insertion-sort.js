fb96b7facafc03ede9ecca5f907b5f378edafe82
Hays Stanford <stanfordhays@gmail.com>
2020-02-06 13:36:05 -0600

    log insertionSort() results for each array test case in test()

--- algorithms/insertion-sort.js
+++ algorithms/insertion-sort.js
@@ -6,6 +6,8 @@ function insertionSort(array) {
     const testArray1 = [4, 5, 2, 1, 3, 8]
     const testArray2 = [5, 5, 6, 100, 3, 5, 2, 1, 5, 7, 8888, 4]
     const testArray3 = [2, 1]
-    
-    console.log('test')
+
+    console.log(insertionSort(testArray1))
+    console.log(insertionSort(testArray2))
+    console.log(insertionSort(testArray3))
 })()
