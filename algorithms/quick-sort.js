ee1d7053c4d757c1734aad06c194f095841f78c8
Hays Stanford <stanfordhays@gmail.com>
2020-02-06 14:27:04 -0600

    added array test cases to test()

--- algorithms/quick-sort.js
+++ algorithms/quick-sort.js
@@ -1,3 +1,7 @@
 (function test() {
+    const testArray1 = [4, 5, 2, 1, 3, 8]
+    const testArray2 = [5, 5, 6, 100, 3, 5, 2, 1, 5, 7, 8888, 4]
+    const testArray3 = [2, 1]
+
     console.log('test')
 })()
