db62357084a3fc1c8cd399f042f6d95d4b7e7626
Hays Stanford <stanfordhays@gmail.com>
2020-02-05 18:00:43 -0600

    added dequeue() to Queue

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -7,6 +7,11 @@ class Queue {
         // Push value to end of queue array
         this.queue.push(value); 
     }
+
+    dequeue() {
+        // Return & delete the first value in queue array
+        return this.queue.shift();
+    }
 }
 
 (function test() {
