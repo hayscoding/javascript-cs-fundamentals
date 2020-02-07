f7f6b2e3f2b80758a31725646536942744f7d0cb
Hays Stanford <stanfordhays@gmail.com>
2020-02-07 15:46:05 -0600

    added mergeSortTopDown() to merge-sort.js

--- algorithms/merge-sort.js
+++ algorithms/merge-sort.js
@@ -1,3 +1,16 @@
+function mergeSortTopDown(array) {
+    if(array.length <= 1) {
+        return array
+    }
+
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+
+    return mergeSortTopDown(mergeSortTopDown(left), mergeSortTopDown(right))
+}
+
+
 (function test() {
     console.log('test')
 })()
