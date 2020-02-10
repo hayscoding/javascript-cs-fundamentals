89ef033b1c615e429f93504c2b0477fe0c47e9f3
Hays Stanford <stanfordhays@gmail.com>
2020-02-10 15:17:25 -0600

    added class Graph with constructor

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -1,3 +1,11 @@
+class Graph {
+    constructor() {
+        this.vertices = [];
+        this.edges = [];
+        this.numberOfEdges = 0;
+    }
+}
+
 (function test() {
     console.log('test')
 })()
