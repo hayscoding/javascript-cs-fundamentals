1b12ef502c4cb39b156b9e20da8b0bed1b0f8e6b
Hays Stanford <stanfordhays@gmail.com>
2020-02-10 15:31:47 -0600

    implemented removeVertex() to remove vertices

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -8,7 +8,23 @@ class Graph {
     addVertex(vertex) {
         this.vertices.push(vertex);
 
-        this.edges
+        this.edges[vertex] = [];
+    }
+
+    removeVertex(vertex) {
+        const index = this.vertices.indexOf(vertex);
+
+        if(index >= 0) {
+            this.vertices.slice(index, 1);
+        }
+
+        // If this vertex has an edge
+        while(this.edges[vertex].length) {
+            // Get last vertex in edge array (will loop until they are empty)
+            const adjacentVertex = this.edges[vertex].pop();
+
+            this.removeEdge(adjacentVertex, vertex);
+        }
     }
 }
 
