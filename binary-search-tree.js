6d0eae13e1aea9d92aa5eb01498aa6944c3bac35
Hays Stanford <stanfordhays@gmail.com>
2020-02-11 14:34:47 -0600

    defined BinarySearchTree class with constructor()

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -3,7 +3,14 @@ function Node(data) {
     this.left, this.right = null;
 }
 
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+}
+
 (function test() {
     let node1 = new Node('Node1')
+
     console.table(node1.right)
 })()
