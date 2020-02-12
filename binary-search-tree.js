9ef0b21be784a3b39afb0a1411b255c0b5aefdc5
Hays Stanford <stanfordhays@gmail.com>
2020-02-11 19:10:22 -0600

    implemented add() for binary search tree

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -7,6 +7,38 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    add(data) {
+        const node = new Node(data);
+
+        if(!this.root) {
+            // Set root node if there are no nodes
+            this.root = node;
+        } else {
+            let current = this.root;
+
+            while(current) {
+                // Nodes data is smaller than current node's data
+                if(node.data < current.data) {
+                    // There is no node at current's left pointer
+                    if(!current.left) {
+                        current.left = node;
+                        break;
+                    }
+                    // Loop until !current.left returns true
+                    current = current.left
+                } else if(node.data > current.data) {
+                    if(!current.right) {
+                        current.right = node;
+                        break;
+                    }
+
+                    current = current.right;
+                } else 
+                    break;
+            }
+        }
+    }
 }
 
 (function test() {
